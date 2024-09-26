15f5c372840620f323b9db021b3311dff81216f4
ArthurBernalSlice <arthur.bernal@slicelife.com>
2024-09-26 18:22:57 +0200

    feat(test): test change version

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_instance_id" {
-  description = "Identifier of the instace that has just been created in AWS EC2"
+  description = "Identifier of the instace that has just been created in AWS EC2, test."
   value =  aws_instance.sample.id
 }
